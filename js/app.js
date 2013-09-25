7aab454b8d2ccf72ca69df615a6eb60c6ef1e535
Sahat Yalkabov <sakhat@gmail.com>
2013-09-24 23:17:04 -0400

    major rework

--- js/app.js
+++ js/app.js
@@ -14,13 +14,15 @@ function showUploadedVideos(data) {
       title: entry.title.$t,
       thumbnail: entry.media$group.media$thumbnail[1].url,
       description: entry.media$group.media$description.$t,
-      viewCount: entry.yt$statistics.viewCount,
+      viewCount: numberWithCommas(entry.yt$statistics.viewCount),
       timeDuration: entry.media$group.yt$duration.seconds,
       raw_uploadDate: entry.media$group.yt$uploaded.$t,
       uploadDate: moment(entry.media$group.yt$uploaded.$t).calendar(),
-      likeCount: entry.yt$rating.numLikes,
-      dislikeCount: entry.yt$rating.numDislikes
+      likeCount: entry.yt$rating.numLikes || 0,
+      dislikeCount: entry.yt$rating.numDislikes || 0
     };
+
+
 
     var minutes = Math.floor(parseInt(item.timeDuration) / 60);
     var seconds = parseInt(item.timeDuration) - minutes * 60;
@@ -29,6 +31,9 @@ function showUploadedVideos(data) {
     var date =  new Date(item.raw_uploadDate);
     item.raw_uploadDate = date.toLocaleDateString();
 
+    var totalVotes = parseInt(item.likeCount, 10) + parseInt(item.dislikeCount, 10);
+    item.percentOfLikes    = 100 * (item.likeCount / totalVotes);
+    item.percentOfDislikes = 100 *  (item.dislikeCount / totalVotes);
     templateData.items.push(item);
   }
 
@@ -90,15 +95,30 @@ function showTumblrPosts(data) {
       question: post.question,
       answer: post.answer,
       note_count: post.note_count,
-      caption: post.caption
+      caption: post.caption,
+      photos: []
     };
-    if (post.photos) {
-      item.photo = post.photos[0].alt_sizes[2].url || ''
+
+    if (item.answer) {
+      var html = $.parseHTML(item.answer);
+      item.answer = $(html).first().text();
+    }
+
+
+    if (post.photos && post.photos.length <= 1) {
+      item.photo = post.photos[0].alt_sizes[2].url || '';
+      item.original_size = post.photos[0].original_size.url || '';
+    } else if (post.photos && post.photos.length > 1) {
+      _.each(post.photos, function(photo) {
+        item.photos.push({
+          url: photo.alt_sizes.slice(-1)[0].url,
+          original_size: photo.original_size.url
+        });
+      });
     }
 
     var temp_date = new Date(item.raw_date);
     item.raw_date = temp_date.toLocaleDateString();
-
     templateData.items.push(item);
   }
 
@@ -107,8 +127,121 @@ function showTumblrPosts(data) {
   $("#main").append(template);
 }
 
-$(function(){
+$.Isotope.prototype._getCenteredMasonryColumns = function() {
+  this.width = this.element.width();
+
+  var parentWidth = this.element.parent().width();
+
+  // i.e. options.masonry && options.masonry.columnWidth
+  var colW = this.options.masonry && this.options.masonry.columnWidth ||
+    // or use the size of the first item
+    this.$filteredAtoms.outerWidth(true) ||
+    // if there's no items, use size of container
+    parentWidth;
+
+  var cols = Math.floor( parentWidth / colW );
+  cols = Math.max( cols, 1 );
+
+  // i.e. this.masonry.cols = ....
+  this.masonry.cols = cols;
+  // i.e. this.masonry.columnWidth = ...
+  this.masonry.columnWidth = colW;
+};
+
+$.Isotope.prototype._masonryReset = function() {
+  // layout-specific props
+  this.masonry = {};
+  // FIXME shouldn't have to call this again
+  this._getCenteredMasonryColumns();
+  var i = this.masonry.cols;
+  this.masonry.colYs = [];
+  while (i--) {
+    this.masonry.colYs.push( 0 );
+  }
+};
+
+$.Isotope.prototype._masonryResizeChanged = function() {
+  var prevColCount = this.masonry.cols;
+  // get updated colCount
+  this._getCenteredMasonryColumns();
+  return ( this.masonry.cols !== prevColCount );
+};
+
+$.Isotope.prototype._masonryGetContainerSize = function() {
+  var unusedCols = 0,
+    i = this.masonry.cols;
+  // count unused columns
+  while ( --i ) {
+    if ( this.masonry.colYs[i] !== 0 ) {
+      break;
+    }
+    unusedCols++;
+  }
+
+  return {
+    height : Math.max.apply( Math, this.masonry.colYs ),
+    // fit container to columns that have been used;
+    width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+  };
+};
+
+
+$(function() {
+
+
+  $('.instagram-link').magnificPopup({
+    type:'image',
+    removalDelay: 500,
+    closeOnContentClick: true,
+    mainClass: 'mfp-fade',
+    image: {
+      verticalFit: true
+    }
+  });
+
+  $('.tumblr-link').magnificPopup({
+    type:'image',
+    removalDelay: 160,
+    closeOnContentClick: true,
+    mainClass: 'mfp-fade',
+    image: {
+      verticalFit: true
+    }
+  });
+
+  $('.tumblr-gallery-link').magnificPopup({
+    type:'image',
+    removalDelay: 160,
+    closeOnContentClick: true,
+    mainClass: 'mfp-fade',
+    image: {
+      verticalFit: true
+    },
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1]
+    }
+  });
+
+
+  $('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
   var $container = $('#main');
+  var $imgs =  $("img.lazy");
+
+  $imgs.lazyload({
+    effect : "fadeIn",
+    failure_limit: Math.max($imgs.length - 1, 0)
+  });
+
   $container.imagesLoaded(function() {
     $container.isotope({
       itemSelector : '.thumbnail',
@@ -118,24 +251,32 @@ $(function(){
         }
       },
       sortBy: 'date',
-      sortAscending : false
+      sortAscending : false,
+      onLayout: function() {
+        $(window).trigger("scroll");
+      }
     });
+    $imgs.load(function () {
+      $container.isotope('reLayout');
+    });
+
 
     var $optionSets = $('#filters'),
       $optionLinks = $optionSets.find('a');
 
     $optionLinks.click(function(){
       var $this = $(this);
-      if ( $this.parent().hasClass('active') ) {
+      if ($this.hasClass('selected'))  {
         return false;
       }
       var $optionSet = $this.parents('#filters');
-      $optionSet.find('.active').removeClass('active');
-      $this.parent().addClass('active');
+      $optionSet.find('.selected').removeClass('selected');
+      $this.addClass('selected');
 
       var options = {},
         key = $optionSet.attr('data-option-key'),
         value = $this.attr('data-option-value');
+
       value = value === 'false' ? false : value;
       options[ key ] = value;
       if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
@@ -148,3 +289,7 @@ $(function(){
     });
   });
 });
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
